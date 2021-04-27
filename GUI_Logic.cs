4d7ba1189891a92fd94fe3ff324c123e7a3dd2db
Noowan <83268235+Noowan@users.noreply.github.com>
2021-04-27 23:23:33 +0500

    Создание stackpanel переехало в класс с логикой GUI

--- GUI_Logic.cs
+++ GUI_Logic.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace NetworkMonitoring
 {
@@ -14,16 +16,32 @@ namespace NetworkMonitoring
         MainWindow Form = Application.Current.Windows[0] as MainWindow;
 
         public static string str_elem_name;
-        public void DeleteStackpanel(string name)
+        public void DeleteStackPanel(string name)
         {
 
-                UIElement elem_name = null;
+            UIElement elem_name = null;
             foreach (StackPanel c in Form.grid1.Children)
                 if (c.Name == name)
                     elem_name = (UIElement)c;
             Form.grid1.Children.Remove(elem_name);
-            
+        }
 
+        public void CreateStackPanel()
+        {
+            var sp = new StackPanel();
+            sp.Name = "dynamic_stackpanel";
+            GUI_Logic.str_elem_name = sp.Name;
+            sp.Children.Add(new TextBlock { Text = "Доступные команды", Width = 130, Height = 30 });
+            sp.Children.Add(new Button { Width = 100, Height = 30, Content = "Опция 1", Name = "Command1" });
+            sp.Children.Add(new Button { Width = 100, Height = 30, Content = "Опция 2", Name = "Command2" });
+            sp.Children.Add(new Button { Width = 100, Height = 30, Content = "Опция 3", Name = "Command3" });
+            sp.Children.Add(new Button { Width = 100, Height = 30, Content = "Опция 4", Name = "Command4" });
+            TranslateTransform transform1 = new TranslateTransform();
+            transform1.X = Mouse.GetPosition(Form.grid1).X - 350;
+            transform1.Y = Mouse.GetPosition(Form.grid1).Y - 100;
+            sp.RenderTransform = transform1;
+            sp.AddHandler(Button.ClickEvent, new RoutedEventHandler(Form.StackPanel_ButtonClick));
+            Form.grid1.Children.Add(sp);
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,52 +24,39 @@ namespace NetworkMonitoring
     public partial class MainWindow : Window
     {
 
-         public MainWindow()
+        public MainWindow()
         {
             InitializeComponent();
         }
 
         public void Window_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
-            
-           //Создаем StackPanel с кнопками и помещаем рядом с курсором
-            var sp = new StackPanel();
-            sp.Name = "dynamic_stackpanel";
-            GUI_Logic.str_elem_name = sp.Name;
-            sp.Children.Add(new TextBlock { Text = "Доступные команды", Width = 130, Height = 30 });
-            sp.Children.Add(new Button { Width = 100, Height = 30, Content = "Опция 1", Name = "Command1" });
-            sp.Children.Add(new Button { Width = 100, Height = 30, Content = "Опция 2", Name = "Command2" });
-            sp.Children.Add(new Button { Width = 100, Height = 30, Content = "Опция 3", Name = "Command3" });
-            sp.Children.Add(new Button { Width = 100, Height = 30, Content = "Опция 4", Name = "Command4" });
-            TranslateTransform transform1 = new TranslateTransform();
-            transform1.X = Mouse.GetPosition(grid1).X - 350;
-            transform1.Y = Mouse.GetPosition(grid1).Y - 100;
-            sp.RenderTransform = transform1;
-            sp.AddHandler(Button.ClickEvent, new RoutedEventHandler(StackPanel_ButtonClick));
-            grid1.Children.Add(sp);
+            //Создаем StackPanel с кнопками и помещаем рядом с курсором
+            GUI_Logic gl = new GUI_Logic();
+            gl.CreateStackPanel();
         }
 
-        private void StackPanel_ButtonClick(object sender, RoutedEventArgs e)
+        public void StackPanel_ButtonClick(object sender, RoutedEventArgs e)
         {
             GUI_Logic gl = new GUI_Logic();
             if (e.Source.ToString().Contains("Опция 1"))
             {
-                gl.DeleteStackpanel(GUI_Logic.str_elem_name);
+                gl.DeleteStackPanel(GUI_Logic.str_elem_name);
             }
 
             if (e.Source.ToString().Contains("Опция 2"))
             {
-                gl.DeleteStackpanel(GUI_Logic.str_elem_name);
+                gl.DeleteStackPanel(GUI_Logic.str_elem_name);
             }
 
             if (e.Source.ToString().Contains("Опция 3"))
             {
-                gl.DeleteStackpanel(GUI_Logic.str_elem_name);
+                gl.DeleteStackPanel(GUI_Logic.str_elem_name);
             }
 
             if (e.Source.ToString().Contains("Опция 4"))
             {
-                gl.DeleteStackpanel(GUI_Logic.str_elem_name);
+                gl.DeleteStackPanel(GUI_Logic.str_elem_name);
             }
 
 
