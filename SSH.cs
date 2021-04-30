c660598e4b63736e25304a8eb7e448e3853ba01a
Noowan <83268235+Noowan@users.noreply.github.com>
2021-04-30 23:28:40 +0500

    Удаление ненужных using

--- SSH.cs
+++ SSH.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using Renci.SshNet;
-using SshNet;
 
 namespace NetworkMonitoring
 {
