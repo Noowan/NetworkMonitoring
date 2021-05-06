d83175029b096695e8dfcfba1020dc8339316af3
Noowan <83268235+Noowan@users.noreply.github.com>
2021-05-06 14:34:29 +0500

    Add alerts.cs

---
+++ Classes/Alerts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NetworkMonitoring.Classes
+{
+    class Alerts
+    {
+        MainWindow Form = Application.Current.Windows[0] as MainWindow;
+
+    }
+}
